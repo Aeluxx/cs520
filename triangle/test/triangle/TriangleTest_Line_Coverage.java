644ce8dc9f7633e1162ef18cfededb196b5d5f17
Aeluxx <matthewgregory@me.com>
2022-03-10 20:50:17 -0500

    Added Line Coverage Test Suite
    Test suite from the history of TriangleTest.java that satisfies Line coverage but nothing else

---
+++ triangle/test/triangle/TriangleTest_Line_Coverage.java
@@ -0,0 +1,61 @@
+package triangle;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import static triangle.Triangle.Type;
+import static triangle.Triangle.Type.*;
+
+/**
+ * Test class for the Triangle implementation.
+ */
+public class TriangleTest_Line_Coverage {
+
+    @Test
+    public void constructorTest() {
+        triangle.Triangle a = new triangle.Triangle();
+        assertNotNull(a);
+    }
+
+    @Test
+    public void trianNonZeroConditional() {
+        Type abSameInvalid = Triangle.classify(1, 1, 2);
+        assertEquals(abSameInvalid, INVALID);
+    }
+
+    @Test
+    public void equilateral() {
+        Type actual = Triangle.classify(10, 10, 10);
+        Type expected = EQUILATERAL;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void invalidTriangle() {
+        Type actualNegative = Triangle.classify(-1, 10, 10);
+        Type actualZero = Triangle.classify(0, 0, 0);
+        Type actualImpossible = Triangle.classify(1, 2, 10);
+        Type expected = INVALID;
+        assertEquals(actualNegative, expected);
+        assertEquals(actualZero, expected);
+        assertEquals(actualImpossible, expected);
+    }
+
+    @Test
+    public void scalene() {
+        Type actual = Triangle.classify(5, 6, 7);
+        Type expected = SCALENE;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void isosceles() {
+        Type expected = ISOSCELES;
+        Type actualAB = Triangle.classify(5, 5, 6);
+        Type actualBC = Triangle.classify(6, 5, 5);
+        Type actualAC = Triangle.classify(5, 6, 5);
+        assertEquals(actualAB, expected);
+        assertEquals(actualBC, expected);
+        assertEquals(actualAC, expected);
+    }
+}
