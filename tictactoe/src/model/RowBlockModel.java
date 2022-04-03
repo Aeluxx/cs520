1778103d37e3a4a23578c54a21bc7ed81f4e3c61
Aeluxx <matthewgregory@me.com>
2022-04-03 03:43:44 -0400

    Update RowBlockModel.java
    Removed unused parts of the code to ensure adherence to proper Object-Oriented programming practices

--- tictactoe/src/model/RowBlockModel.java
+++ tictactoe/src/model/RowBlockModel.java
@@ -16,25 +16,20 @@ public class RowBlockModel
     private String contents;
 
     /**
-     * Whether or not it is currently legal to move into this block
-     */
-    private boolean isLegalMove;
-
-    /**
      * Creates a new block that will be contained in the given game.
      *
      * @param game The game that will contain the new block
      * @throws IllegalArgumentException When the given game is null
      */
     public RowBlockModel(RowGameModel game) {
-	super();
+        super();
 
-	if (game == null) {
-	    throw new IllegalArgumentException("The game must be non-null.");
-	}
-	
-	this.game = game;
-	this.reset();
+        if (game == null) {
+            throw new IllegalArgumentException("The game must be non-null.");
+        }
+
+        this.game = game;
+        this.reset();
     }
 
     public RowGameModel getGame() {
@@ -48,10 +43,10 @@ public class RowBlockModel
      * @throws IllegalArgumentException When the given value is null
      */
     public void setContents(String value) {
-	if (value == null) {
-	    throw new IllegalArgumentException("The value must be non-null.");
-	}
-	this.contents = value;
+        if (value == null) {
+            throw new IllegalArgumentException("The value must be non-null.");
+        }
+        this.contents = value;
     }
 
     /**
@@ -63,19 +58,10 @@ public class RowBlockModel
 	return this.contents;
     }
 
-    public void setIsLegalMove(boolean isLegalMove) {
-	this.isLegalMove = isLegalMove;
-    }
-
-    public boolean getIsLegalMove() {
-	return this.isLegalMove;
-    }
-
     /**
      * Resets this block before starting a new game.
      */
     public void reset() {
-	this.contents = "";
-	this.isLegalMove = false;
+        this.contents = "";
     }
 }
